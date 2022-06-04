1104a9930ed52adca393516dcd89cef6697c2bd8
Falence Lemungoh <falencelemungoh@gmail.com>
2022-06-04 14:43:49 +0100

    implement the add new topic from frontend functionality

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """The home page for Learning Log."""
@@ -15,6 +16,22 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
+    entries = topic.entry_set.order_by('-date_added')   # order by newest entries created
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
